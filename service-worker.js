c30ce291ed8f126952f8bb1a1b142742cf96d7d3
qoo109 <9s5nmzrjvj@privaterelay.appleid.com>
2026-07-09 11:34:58 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "steampunk-air-hockey-v1-1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./game.js",
+  "./manifest.json",
+  "./icon.svg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});

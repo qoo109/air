7e4fab3eb6a6ae389285c7b1b8683d5dab613cae
qoo109 <9s5nmzrjvj@privaterelay.appleid.com>
2026-07-17 02:19:04 +0800

    chore: refresh Bubble Island cache v2.3

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,23 @@
-const CACHE_NAME = "bubble-island-air-hockey-v2-2";
+const CACHE_NAME = "bubble-island-air-hockey-v2-3";
 const ASSETS = [
   "./",
   "./index.html",
-  "./style.css?v=2.2",
-  "./guest.css?v=2.2",
-  "./fullscreen.js?v=2.2",
+  "./style.css?v=2.3",
+  "./guest.css?v=2.3",
+  "./fullscreen.js?v=2.3",
   "./auth.js?v=2.1",
   "./game.js?v=2.0",
   "./manifest.json",
   "./icon.svg"
 ];
-
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
-
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))));
   self.clients.claim();
 });
-
 self.addEventListener("fetch", event => {
   event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });

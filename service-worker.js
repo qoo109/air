745d284897c070d87fca9681ab8f67f5d16e1986
qoo109 <9s5nmzrjvj@privaterelay.appleid.com>
2026-07-17 02:24:27 +0800

    fix: retire stale service worker cache

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,17 @@
-const CACHE_NAME = "bubble-island-air-hockey-v2-3";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./style.css?v=2.3",
-  "./guest.css?v=2.3",
-  "./fullscreen.js?v=2.3",
-  "./auth.js?v=2.1",
-  "./game.js?v=2.0",
-  "./manifest.json",
-  "./icon.svg"
-];
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+self.addEventListener('install', event => {
   self.skipWaiting();
 });
-self.addEventListener("activate", event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))));
-  self.clients.claim();
+
+self.addEventListener('activate', event => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map(key => caches.delete(key)));
+    await self.registration.unregister();
+    const clients = await self.clients.matchAll({ type: 'window' });
+    for (const client of clients) client.navigate(client.url);
+  })());
 });
-self.addEventListener("fetch", event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
 });

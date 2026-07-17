dcace78463b01e2917b5446235b9251c3bda8764
qoo109 <9s5nmzrjvj@privaterelay.appleid.com>
2026-07-17 14:34:21 +0800

    Enable Supabase global leaderboard

--- cloud-config.js
+++ cloud-config.js
@@ -1,5 +1,5 @@
 window.BUBBLE_CLOUD_CONFIG = {
-  enabled: false,
-  supabaseUrl: '',
-  supabasePublishableKey: ''
+  enabled: true,
+  supabaseUrl: 'https://upncojrpmeopdqlwzge.supabase.co',
+  supabasePublishableKey: 'sb_publishable_jMwyUCEu2v5tpUeu1YnwYw__CPt7SqH'
 };
